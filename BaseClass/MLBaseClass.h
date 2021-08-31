f1afe3870fcb151031cab5f9565f4f6097b5ee92
Milo <Milo.wei@honexun.com>
2021-08-31 09:53:19 +0800

    update

---
+++ BaseClass/MLBaseClass.h
@@ -0,0 +1,18 @@
+//
+//  MLBaseClass.h
+//  text
+//
+//  Created by 魏明磊 on 2021/8/31.
+//
+
+#ifndef MLBaseClass_h
+#define MLBaseClass_h
+#import "heads.h"
+#import "MLBaseViewController.h"
+#import "MLBaseTableViewCell.h"
+#import "MLBaseView.h"
+#import "MLBaseTableViewSectionModel.h"
+#import "MLBaseCellModel.h"
+#import "MLBaseTableViewCell.h"
+#import "MLBaseNavigationController.h"
+#endif /* MLBaseClass_h */
